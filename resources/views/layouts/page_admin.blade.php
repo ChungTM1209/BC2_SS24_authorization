7ef4d8cabd2a04f574709d1c75d518901620cb86
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-03-05 08:40:31 +0700

    BC2_SS24_authorization

---
+++ resources/views/layouts/page_admin.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chungtran
+ * Date: 05/03/2019
+ * Time: 08:27
+ */
+echo "xin chao admin";
+
---
+++ resources/views/layouts/page_guest.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chungtran
+ * Date: 05/03/2019
+ * Time: 08:26
+ */
+echo "xin chao guest";
+
